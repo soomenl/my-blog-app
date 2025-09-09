b6ce3ec25444a2355b64cf9dfabf4425ca13e00d
min20 <min20993@gmail.com>
2025-09-09 19:35:43 +0900

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    basePath: 'my-blog-app',
 };
 
 export default nextConfig;
